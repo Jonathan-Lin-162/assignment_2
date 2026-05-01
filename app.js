425a50c6b1bc90dc29f8a20712321f8077cc7a3e
Jonathan-Lin-162 <ylin297@my.bcit.ca>
2026-05-01 00:52:55 -0700

    Added required features to the app for assignment

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ require("./utils.js");
 require("dotenv").config();
 const express = require("express");
 const session = require("express-session");
+const MongoStore = require("connect-mongo").default;
 const bcrypt = require("bcrypt");
 const Joi = require("joi");
 const port = process.env.port || 3000;
@@ -23,8 +24,25 @@ const userCollection = database
   .db(mongodb_user_database)
   .collection("user_collection");
 
+app.use(express.static(__dirname + "/public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+const mongoStore = MongoStore.create({
+  mongoUrl: `mongodb+srv://${mongodb_user}:${encodeURIComponent(mongodb_password)}@${mongodb_host}/${mongodb_session_database}`,
+  crypto: {
+    secret: mongodb_session_secret,
+  },
+});
+
+app.use(
+  session({
+    secret: node_session_secret,
+    store: mongoStore,
+    saveUninitialized: false,
+    resave: true,
+  }),
+);
 
 app.get("/", (req, res) => {
   res.send(`
@@ -66,12 +84,12 @@ app.post("/signupSubmit", async (req, res) => {
     return res.send(`Password is required. <a href="/signUp">Try again</a>`);
   }
   const schema = Joi.object({
-    name: Joi.string().alphanum().max(20).required(),
+    username: Joi.string().alphanum().max(20).required(),
     email: Joi.string().max(20).required(),
     password: Joi.string().max(20).required(),
   });
 
-  const result = schema.validate(req.body);
+  const result = schema.validate({ username, email, password });
   if (result.error != null) {
     return res.send(`Invalid input. <a href="/signUp">Try again</a>`);
   }
@@ -86,6 +104,7 @@ app.post("/signupSubmit", async (req, res) => {
 
   req.session.username = username;
   req.session.email = email;
+  req.session.authenticated = true;
 
   res.redirect("/members");
 });
@@ -99,11 +118,74 @@ app.get("/login", (req, res) => {
         <input type="submit" value="submit">
         </form>
         `;
+  res.send(html);
 });
 
-app.post("/loginSubmit", (req, res) => {
+app.post("/loginSubmit", async (req, res) => {
   const email = req.body.email;
   const password = req.body.password;
+
+  const schema = Joi.object({
+    email: Joi.string().max(20).required(),
+    password: Joi.string().max(20).required(),
+  });
+
+  const validationResult = schema.validate({ email, password });
+  if (validationResult.error != null) {
+    return res.send(
+      `Invalid email/password combination <a href="/login">Try again</a>`,
+    );
+    return;
+  }
+
+  const result = await userCollection
+    .find({ email: email })
+    .project({ username: 1, email: 1, password: 1, _id: 1 })
+    .toArray();
+  if (result.length != 1) {
+    console.log("user not found");
+    res.redirect("/login");
+    return;
+  }
+  if (await bcrypt.compareSync(password, result[0].password)) {
+    req.session.authenticated = true;
+    req.session.username = result[0].username;
+    req.session.cookie.maxAge = expireTime;
+
+    res.redirect("/members");
+    return;
+  } else {
+    console.log("incorrect password");
+    res.redirect("/login");
+    return;
+  }
+});
+
+app.get("/logout", (req, res) => {
+  req.session.destroy();
+  res.redirect("/");
+});
+
+app.get("/members", (req, res) => {
+  if (!req.session.authenticated) {
+    res.redirect("/");
+  }
+
+  const images = ["image1.jpg", "image2.jpg", "image3.jpg"];
+  const randomImage = images[Math.floor(Math.random() * images.length)];
+
+  res.send(`
+      <h2>Hello, ${req.session.username}.</h2>
+      <img src=image/${randomImage} alt="image" width="300px">
+      <a href="/logout">
+      <button>Logout</button>
+      </a>  
+    `);
+});
+
+app.use((req, res) => {
+  res.status(404);
+  res.send("page not found - 404");
 });
 
 app.listen(port, (req, res) => {
